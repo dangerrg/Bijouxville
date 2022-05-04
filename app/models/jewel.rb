d97ba36131f471c50fd42b62bfd40130fdae49af
dangerrg <39160873+dangerrg@users.noreply.github.com>
2022-05-05 00:02:44 +0200

    Add validations to the jewel class

--- app/models/jewel.rb
+++ app/models/jewel.rb
@@ -2,4 +2,10 @@ class Jewel < ApplicationRecord
   belongs_to :jeweler
   has_many :stones
   has_many :materials
+
+  validates :name, presence: true
+  validates :number_of_stones, presence: true
+  validates :type_of_stones, presence: true
+  validates :material, presence: true
+  validates :jeweler, presence: true
 end
